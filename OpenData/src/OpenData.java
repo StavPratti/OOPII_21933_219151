ab6efc9c60bdc7d75bd4c631b464e6c1fbbfeebe
User <User@LAPTOP-VSGIM3VI>
2021-03-24 10:41:46 +0200

    project

--- OpenData/src/OpenData.java
+++ OpenData/src/OpenData.java
@@ -1,10 +1,11 @@
 import java.io.IOException;
 import java.net.URL;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import weather.OpenWeatherMap; 
+//import weather.OpenWeatherMap; 
 import wikipedia.MediaWiki;
-import static moreinfo.CountWords.*;
-
+import totravel.City;
+//import static moreinfo.CountWords.*;
+import totravel.City.*;
 /**City description and weather information using OpenData with Jackson JSON processor.
 * @since 29-2-2020
 * @version 1.0
@@ -21,15 +22,20 @@ public class OpenData {
 	// System.out.println(city+" temperature: " + (weather_obj.getMain()).getTemp());
 	// System.out.println(city+" lat: " + weather_obj.getCoord().getLat()+" lon: " + weather_obj.getCoord().getLon());
 	 MediaWiki mediaWiki_obj =  mapper.readValue(new URL("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles="+city+"&format=json&formatversion=2"),MediaWiki.class);
-	 //System.out.println(city+" Wikipedia article: "+mediaWiki_obj.getQuery().getPages().get(0).getExtract());	 
-	 int count=countCriterionfCity(mediaWiki_obj.getQuery().getPages().get(0).getExtract(),"walls");
-	 System.out.println(count);
+	System.out.println(city+" Wikipedia article: "+mediaWiki_obj.getQuery().getPages().get(0).getExtract());	 
+	 //int count=countCriterionfCity(mediaWiki_obj.getQuery().getPages().get(0).getExtract(),"walls");
+	 //System.out.println(count);
 }
 
- 
+ //https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=Rome&format=json&formatversion=2 στο google το αρθρο τησ wikipedia for rome
 public static void main(String[] args) throws IOException {
-	String appid ="c01b30fa264932378fe076eb38121dbc";
-	RetrieveData("Rome","it",appid);	
+	//String appid ="c01b30fa264932378fe076eb38121dbc";
+	City city1=new City("Rome","it");//παρομοια μπορω να φτιαξω και αλλες πολεις
+	city1.CityTerms();//για την συγκεκριμενη πολη φτιαχνει τα τερμσ
+	city1.CityLanLot();//το τατ λοτ, ωστε με γκετερς να μπορω να τα χρησιμοποιησω μετα
+	System.out.println(city1.getGeodestic_vector(0));//lat γενικα ετσι μπορω να παρω ολα τα στοιχεια του πινακα
+		
+	//RetrieveData("Rome","it",appid);	
 	/*RetrieveData("Athens","gr",appid);
 	RetrieveData("Corfu","gr",appid);	
 	RetrieveData("Berlin","de",appid);	*/
---
+++ OpenData/src/totravel/City.java
@@ -0,0 +1,124 @@
+package totravel;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static moreinfo.CountWords.*;
+import weather.OpenWeatherMap;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import wikipedia.MediaWiki;
+
+public class City {
+
+	private String city;
+	private int[] terms_vector=new int[10];
+	private double[] geodestic_vector=new double[2];
+	private String country;
+	
+
+	public City(String city,String country) {
+		this.city=city;	
+		this.country=country;
+		
+	}
+	
+	/**Retrieves weather information, geotag (lan, lon) and a Wikipedia article for a given city.
+	* @param city The Wikipedia article and OpenWeatherMap city. 
+	* @param country The country initials (i.e. gr, it, de).
+	* @param appid Your API key of the OpenWeatherMap.
+	 * @throws IOException 
+	 * @throws MalformedURLException 
+	 * @throws JsonMappingException 
+	 * @throws JsonParseException */ 
+	//φτιαχνει τον πίνακα terms_vector
+	public void CityTerms() throws JsonParseException, JsonMappingException, MalformedURLException, IOException{
+	 
+		
+     String[] terms_vector_strings=new String[]{"sea","walls","ancient","mountain","brige","museum","squeare","restaurant","view","forest"};;
+	 ObjectMapper mapper = new ObjectMapper(); 
+	// int[] terms_vector=new int[10];
+	 
+	 
+	 MediaWiki mediaWiki_obj =  mapper.readValue(new URL("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles="+city+"&format=json&formatversion=2"),MediaWiki.class);
+	 //System.out.println(city+" Wikipedia article: "+mediaWiki_obj.getQuery().getPages().get(0).getExtract());
+	 for(int i=0;i<terms_vector_strings.length;i++) {
+		 int count=countCriterionfCity(mediaWiki_obj.getQuery().getPages().get(0).getExtract(),terms_vector_strings[i]);
+		 terms_vector[i]=count;
+	  }
+	}
+	 
+	 /*for(int i=0;i<terms_vector.length;i++) {
+		System.out.println(terms_vector[i]); 
+	 }
+	}*/
+	
+//φτιαχνει τον πίνακα geodestic_vector
+	public void CityLanLot() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+		ObjectMapper mapper2=new ObjectMapper();
+		//String country="it";
+		//double[] geodestic_vector=new double[2];
+		String appid="c01b30fa264932378fe076eb38121dbc";
+		OpenWeatherMap weather_obj = mapper2.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&APPID="+appid+""), OpenWeatherMap.class);
+		geodestic_vector[0]=weather_obj.getCoord().getLat();
+		geodestic_vector[1]=weather_obj.getCoord().getLon();
+		//System.out.println("lat:"+geodestic_vector[0]+"\tlon"+geodestic_vector[1]);
+	}
+	
+	
+	
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+		
+		
+		//CityTerms("Rome"); //δίνω πόλη και μου φτιαχνει το πινακα με το ποσες φορες βρικε τισ παρα πανω λεξεις στο κειμενο της πολης
+		//CityLanLot("Rome");
+	}
+	
+	
+	public int getTerms_vector(int index) {
+		return terms_vector[index];
+	}
+
+	public void setTerms_vector(int[] terms_vector) {
+		this.terms_vector = terms_vector;
+	}
+
+	public double getGeodestic_vector(int index) {
+		
+		return geodestic_vector[index];
+	}
+
+	public void setGeodestic_vector(double[] geodestic_vector) {
+		this.geodestic_vector = geodestic_vector;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
